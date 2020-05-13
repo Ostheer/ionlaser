b3f5d5552661479bd2e6b6fa9267f79ae3bdbeba
Ostheer <mathijs@verhaegh.nl>
2020-05-13 15:02:35 +0200

    POC 3

--- displaycontrol/displaycontrol.ino
+++ displaycontrol/displaycontrol.ino
@@ -1,6 +1,6 @@
 /* Timing constants */
-#define CLKPWMRATIO 100 // Amount of clock cycles per display update cycle
-#define UPDATECOUNT 1000 // Amount of clock cycles per frame. One frame takes CLKPWMRATIO*UPDATECOUNT clock cycles
+#define DL 100 // Amount of clock cycles per display update cycle = Display Length / resolution
+#define UPDATECOUNT 1000 // Amount of clock cycles per frame. One frame takes DL*UPDATECOUNT clock cycles
 
 /* Pin definitions */
 #define PWMPIN D1
@@ -14,28 +14,48 @@ int s = 0;
 int a = 0;
 
 /* Framebuffer */
-bool buffer[CLKPWMRATIO] = {};
+bool buffer[DL] = {};
 
 void setup() {
+  /* Initialise pins */
   pinMode(PWMPIN,OUTPUT);
   pinMode(CLKPIN,OUTPUT);
   pinMode(RSTPIN,INPUT);
   digitalWrite(RSTPIN,LOW);
-  flush();
 
-  //set segment permanently on
-  //for (int i = 20; i<40; i++) buffer[i]=true;
+  delay(5000);
+  /* Flush the counter IC's until they reset */
+  for (int i = 0; i < DL; i++){
+    digitalWrite(CLKPIN,LOW);
+    delayMicroseconds(10);
+    if (digitalRead(RSTPIN)) break;
+    delayMicroseconds(10);
+    digitalWrite(CLKPIN,HIGH); 
+    delayMicroseconds(20);
+  }
 }
   
 void loop() {
   /* Write buffer */
   digitalWrite(PWMPIN,!buffer[k]);
   
-  /* Do animation */
-  //buffer[a] = !buffer[a]; //fill and clear
-  //buffer[a]=true; if (a>0) buffer[a-1]=false; else buffer[CLKPWMRATIO]=false; //walking line
-  if (a>CLKPWMRATIO) a=0;
-  
+  /* Animations */
+  //fill and clear
+  //if (a>DL) a=0;
+  //buffer[a] = !buffer[a]; 
+
+  //walking line
+  //if (a>DL) a=0;
+  //buffer[a]=true; if (a>0) buffer[a-1]=false; else buffer[DL]=false; 
+
+  //centered pulse
+  if (a>2*DL/3) a=0;
+  else if (a>DL/3){ buffer[DL/2-DL/3+(a-DL/3)-1] = false; buffer[DL/2+DL/3-(a-DL/3)+1] = false; }
+  else{ buffer[DL/2-a]=true; buffer[DL/2+a]=true; }
+
+  //set segment permanently on
+  //for (int i = 20; i<40; i++) buffer[i]=true;
+
   /* Take care of Animation update rate */
   s++;
   if (s>UPDATECOUNT) {a++;s=0;}
@@ -43,20 +63,8 @@ void loop() {
   /* Generate clock signal */
   if (clk) {
     k++;
-    if (k == CLKPWMRATIO) k = 0;
+    if (k == DL) k = 0;
   }
   clk = !clk;
   digitalWrite(CLKPIN,clk);
-}
-
-/* Flush the counter IC's until they reset */
-void flush() {
-  for (int i = 0; i < CLKPWMRATIO; i++){
-    digitalWrite(CLKPIN,LOW);
-    delayMicroseconds(10);
-    if (digitalRead(RSTPIN)) return;
-    delayMicroseconds(10);
-    digitalWrite(CLKPIN,HIGH); 
-    delayMicroseconds(20);
-  }
 }
